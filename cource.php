bb31c4eec5a76d6733caf57a5b8f3d7fd09bf7b7
polina <polina@mail.ru>
2022-11-11 12:12:56 +0800

    1

---
+++ cource.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Дисциплины</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <a class="nav__link" href="index.php">Главная</a>
+            <a class="nav__link" href="college.php">О техникуме</a>
+            <a class="nav__link" href="cource.php">Дисциплины</a>
+        </nav>
+    </header>
+    <div class="page">
+        <section class="section">
+            <h2 class="section__title">Дисциплины</h2>
+            <a href="1.php">Компьютерная графика</a>
+            <a href="2.php">Веб-программирование</a>
+            <a href="3.php">Основы алгоритмизации и программирования</a>
+            <a href="4.php">Компьютерные сети</a>
+            <a href="#">5</a>
+            <a href="#">6</a>
+            <a href="#">7</a>
+        </section>
+    </div>
+    <p>Страница сделана Родионом Браун</p>
+</body>
+</html>
---
+++ index (1).php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная (Костя)</title>
+</head>
+<body>
+    <h1>Рассказ о группе И-19-1</h1>
+    <p>Сущесвтует в городе Ангарск такой замечательный техникум "АПТ"</p>
+</body>
+</html>
